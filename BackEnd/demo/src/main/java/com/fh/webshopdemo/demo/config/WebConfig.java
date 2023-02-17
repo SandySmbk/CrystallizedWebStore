b43003039a06cf1e7822824b94b4ae114b4ab3df
SandrinaSmbk <sandrina.schirmboeck@gmx.at>
2023-02-17 11:41:16 +0100

    test

--- BackEnd/demo/src/main/java/com/fh/webshopdemo/demo/config/WebConfig.java
+++ BackEnd/demo/src/main/java/com/fh/webshopdemo/demo/config/WebConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+//CORS ist ein Mechanismus, der es Webanwendungen ermöglicht, auf Ressourcen von einer anderen Domäne zuzugreifen, 
+//als die Domäne, auf der die Webanwendung gehostet wird. Dies ist ein wichtiges Konzept in der heutigen Webentwicklung,
+// da viele Webanwendungen mit Ressourcen von Drittanbietern arbeiten, um bestimmte Funktionen zu ermöglichen.
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
